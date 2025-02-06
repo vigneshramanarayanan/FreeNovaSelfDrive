8507bd6924020e13976bd4b7f6f69b0f17e46503
vigneshramanarayanan <viggi85@gmail.com>
2025-02-05 19:29:21 -0600

    Add files via upload

--- Vignesh_FreeNova_Test.py
+++ Vignesh_FreeNova_Test.py
@@ -14,11 +14,11 @@ def KeepRunningTillObstacle():
 	while(data>50):	# if more than 30 cm
 		data=ultrasonic.get_distance()   #Get the value
 		print ("Obstacle distance is "+str(data)+"CM")
-		motorSpeed = 900
+		motorSpeed = 1000
 		PWM.setMotorModel(motorSpeed,motorSpeed,motorSpeed,motorSpeed)
 	PWM.setMotorModel(0,0,0,0)
 	time.sleep(1)
-	PWM.setMotorModel(-500,-500,-500,-500) #Reverse Car
+	PWM.setMotorModel(-600,-600,-600,-600) #Reverse Car
 	time.sleep(1)
 	PWM.setMotorModel(0,0,0,0)
 	time.sleep(1)
@@ -36,7 +36,7 @@ def TurnLeft():
 def TurnRight():
 	pwm.setServoPwm('0',90) #Set Servo 0 to look Straight	
 	PWM.setMotorModel(800,800,-600,-600)       #Turn Right 
-	print ("The car is turning left") 
+	print ("The car is turning Right") 
 	time.sleep(2)
 	PWM.setMotorModel(0,0,0,0)       #Stop Motor
 	time.sleep(1)			
@@ -50,11 +50,11 @@ def StartCar():
 		KeepRunningTillObstacle()				
 		#Obsctacle met		
 		
-		pwm.setServoPwm('0',40)	 #See left if there is space
+		pwm.setServoPwm('0',30)	 #See left if there is space
 		time.sleep(1)
 		dataLeft=ultrasonic.get_distance()   #Get the Object in front value
 		print ("Obstacle Left distance is "+str(dataLeft)+"CM")
-		pwm.setServoPwm('0',140)	
+		pwm.setServoPwm('0',150)	
 		time.sleep(1)
 		dataRight=ultrasonic.get_distance()   #Get the Object in front value
 		print ("Obstacle Right distance is "+str(dataRight)+"CM")
